ca420dddb2c49beb7bbc31a9f3a5cd9d37df7dec
amarNrddn <amarnrddn02@gmail.com>
2024-01-14 17:59:06 +0700

    setup controller todo

--- app/api/todos/controller.js
+++ app/api/todos/controller.js
@@ -1,4 +1,5 @@
 const { Todo, Item } = require('../../db/models')
+const { response } = require('./router')
 
 const getAll = async (req, res, next) => {
     try {
@@ -8,7 +9,6 @@ const getAll = async (req, res, next) => {
                 model: Item,
                 attributes: ['id', 'name', 'TodoId'],
             }
-
         })
         res.status(200).json({
             message: 'Succes',
@@ -19,6 +19,68 @@ const getAll = async (req, res, next) => {
     }
 }
 
+const showOne = async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const result = await Todo.findOne({
+            where: { id: id }
+        })
+
+        res.status(200).json({
+            message: 'Succes',
+            data: result
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+const create = async (req, res, next) => {
+    try {
+        const { name } = req.body
+        const result = await Todo.create({ name })
+
+        res.status(201).json({
+            message: 'Succes',
+            data: result
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+const update = async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const { name } = req.body
+
+        const chechk = await Todo.findOne({
+            where: { id: id }
+        })
+
+        if (!chechk) {
+            res.status(400).json({
+                message: `Tidak ada id ${id}`
+            })
+        }
+
+        const result = await Todo.update(
+            { name },
+            { where: { id: id } }
+        )
+
+        res.status(201).json({
+            message: 'Succes',
+            data: result
+        })
+    } catch (error) {
+        next((error))
+    }
+}
+
 module.exports = {
-    getAll
+    getAll,
+    create,
+    showOne,
+    update
 }
--- app/api/todos/router.js
+++ app/api/todos/router.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const route = express()
-const { getAll } = require('./controller')
+const { getAll, create, showOne, update } = require('./controller')
 
 route.get('/todos', getAll)
+route.get('/todos/:id', showOne)
+route.post('/todos', create)
+route.put('/todos/:id', update)
 
 module.exports = route
